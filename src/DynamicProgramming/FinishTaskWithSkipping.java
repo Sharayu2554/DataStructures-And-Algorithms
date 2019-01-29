980c6588d09028a182cb45a485c0c16d36aa39bd
sharayu <sharayumantri2554@gmail.com>
2019-01-29 15:51:07 -0600

    Implemented finish with skipping but consecutive skipping not allowed, OPtimized solution

--- src/DynamicProgramming/FinishTaskWithSkipping.java
+++ src/DynamicProgramming/FinishTaskWithSkipping.java
@@ -37,22 +37,46 @@ public class FinishTaskWithSkipping {
         return skip(arr, 0, -2, -2, 0);
     }
 
+    //Run time : O(N)
+    public static int skipOptimized(int[] arr) {
+        if (arr.length == 0)
+            return 0;
+
+        int inc = arr[0];
+        int exc = 0;
+
+        int inc_n , exc_n;
+        for (int i = 1; i< arr.length; i++) {
+            inc_n = arr[i] + Math.min(inc, exc);
+            exc_n = inc;
+
+            inc = inc_n;
+            exc = exc_n;
+        }
+
+        return Math.min(inc, exc);
+    }
+
     public static void main(String args[]) {
 
         int[] arr = {10, 5, 2, 4, 8, 6, 7, 10};
         System.out.println(" Result " + skip(arr));
+        System.out.println(" Result " + skipOptimized(arr));
         //Expected 22
 
         int[] arr1 = { 10, 5, 7, 10};
         System.out.println(" Result " + skip(arr1));
+        System.out.println(" Result " + skipOptimized(arr1));
         //Expected Output 12
 
         int[] arr2 = { 10, 5, 2, 7, 10};
         System.out.println(" Result " + skip(arr2));
+        System.out.println(" Result " + skipOptimized(arr2));
         //Expected output 12
 
         int[] arr3 = {10, 10, 5, 2, 4, 8, 6, 10, 7 ,10 };
         System.out.println(" Result " + skip(arr3));
+        System.out.println(" Result " + skipOptimized(arr3));
         //Expected Output 29
     }
 }
