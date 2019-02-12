059fb7611f802f7921141d3d96aa0d53ef1ad6ee
sharayu <sharayumantri2554@gmail.com>
2019-02-12 13:58:14 -0600

    Local Min and MAx - fix optimized O(logN) solution

--- src/NumberProblems/LocalMinMax.java
+++ src/NumberProblems/LocalMinMax.java
@@ -14,97 +14,85 @@ import java.util.List;
 
 public class LocalMinMax {
 
-    public static void localMaxMin(int[] arr) {
+    class Node {
+        int l;
+        int r;
+
+        public Node(int i, int j) {
+            l = i;
+            r = j;
+        }
+    }
+
+    public void localMaxMin(int[] arr) {
 
         System.out.println("\nInput " + Arrays.toString(arr));
-        int flag = 0; //1 = max, -1 = min, 0 = none
-        int i =0;
-        int n = arr.length;
         List<Integer> res = new LinkedList<>();
-
-        while (i < n) {
-
-            if (flag == 0) {
-
-                if (i+2 >= n) {
-                    break;
-                }
-                else if (arr[i] == arr[i+2]) {
-                    if (arr[i+1] < arr[i])
-                        flag = -1;
-                    else
-                        flag = 1;
-
-                    res.add(i+1);
-                    i = i +1;
-                }
-                else {
-                    flag = 0;
-                    i = i + 1;
-                }
-            }
-            else if(flag  == 1) {
-                if (i+3 >= n) {
-                    flag = 0;
-                    continue;
-                }
-                else if (arr[i+3] == (arr[i] -1)) {
-                    if (arr[i] == arr[i+2])
-                        res.add(i+1);
-
-                    flag = -1;
-                    res.add(i+2);
-                    i = i + 2;
-                }
-                else {
-                    flag = 0;
-                    i = i + 2;
-                }
-
-            }
-            else if (flag == -1) {
-                if (i+3 >= n) {
-                    flag = 0;
-                    continue;
-                }
-                else if (arr[i+3] == (arr[i] +1)) {
-                    if (arr[i] == arr[i+2])
-                        res.add(i+1);
-
-                    flag = 1;
-                    res.add(i+2);
-                    i = i + 2;
-                }
-                else {
-                    flag = 0;
-                    i = i + 2;
-                }
+        if (arr.length > 2) {
+            if ((arr[0] + arr.length - 1 != arr[arr.length -1]) ||
+                 arr[0] - arr.length + 1 != arr[arr.length -1]) {
+                localRecursion(arr, 0, arr.length-1, res);
             }
         }
+
         System.out.println(Arrays.toString(res.toArray()));
     }
 
+    public Node localRecursion(int[] arr, int p, int q, List<Integer> res) {
+
+        if (p > q)
+            return null;
+        if (p == q)
+            return new Node(0, 0);
+
+        if (q -p == 1) {
+            if  (arr[p] < arr[q])
+                return new Node(1, 1);
+            else if (arr[p] > arr[q])
+                return new Node(-1, -1);
+            else
+                return new Node(0, 0);
+        }
+
+        int m = (p + q)/2;
+        Node L = localRecursion(arr, p, m, res);
+        Node R = localRecursion(arr, m, q, res);
+
+        if (L.r == -1 && R.l == 1) {
+            res.add(m);
+        }
+        else if (L.r == 1 && R.l == -1) {
+            res.add(m);
+        }
+        return new Node(L.l, R.r);
+    }
+
     public static void main(String args[]) {
+
+        LocalMinMax l = new LocalMinMax();
+        int[] input0 = {1, 2, 1, 2, 3, 2, 1, 2, 3, 4};
+        l.localMaxMin(input0);
+
         int[] input = {9, 10, 9, 8, 9, 10, 11, 12, 11, 10, 9, 8, 7, 8, 9, 8, 7, 8, 9};
-        localMaxMin(input);
+        l.localMaxMin(input);
 
         int[] input1 = { 1 };
-        localMaxMin(input1);
+        l.localMaxMin(input1);
 
         int[] input2 = {1, 2};
-        localMaxMin(input2);
+        l.localMaxMin(input2);
 
         int[] input3 = {1, 2, 3};
-        localMaxMin(input3);
+        l.localMaxMin(input3);
 
         int[] input4 = {1, 2, 3, 4, 5, 6};
-        localMaxMin(input4);
+        l.localMaxMin(input4);
 
         int[] input5 = {1, 2, 1};
-        localMaxMin(input5);
+        l.localMaxMin(input5);
 
         int[] input6 = {1, 2, 1, 0};
-        localMaxMin(input6);
+        l.localMaxMin(input6);
 
     }
 }
